1123ed0b893f0a912949496e96a56ceb528a9a56
tq <106814858+caotrungquan24092007@users.noreply.github.com>
2023-02-03 19:58:01 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
 var orders;
 
-// function getData() {
-  
-
-// }
-
-// var kkk = (sessionStorage.getItem("key") || []).reduce((prev, curr) => {
-//   return prev + curr.product + ", qty: " + curr.qty + "; ";
-// }, "")
-
 function sendMail() {
 
-  // console.log(kkk)
   let orders = sessionStorage.getItem("Furniture-cart")
   var params = {
     name: document.getElementById("name").value,
@@ -21,15 +11,40 @@ function sendMail() {
     product: orders,
 
   };
-  // kkk = sessionStorage.getItem("key").reduce((prev, curr) => {
-  //   return prev + curr.product + ", qty: " + curr.qty + "; ";
-  // }, "");
-  // console.log(params);
-  // sessionStorage.setItem('Data',params.items);
-
 
   const serviceID = "service_ua2pklx";
   const templateID = "template_3cp7zy7";
+
+    emailjs.send(serviceID, templateID, params)
+    .then(res=>{  
+        // let data = sessionStorage.getItem("key");
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("phone").value = "";
+        document.getElementById("message").value = "";
+
+        console.log(res);
+        alert("Gửi thành công !!!")
+
+    })
+    .catch(err=>console.log(err));
+
+}
+
+function sendMail2() {
+
+  // let orders = sessionStorage.getItem("Furniture-cart")
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    phone: document.getElementById("phone").value,
+    message: document.getElementById("message").value,
+    // product: orders,
+
+  };
+
+  const serviceID = "service_ua2pklx";
+  const templateID = "template_vfnq5ci";
 
     emailjs.send(serviceID, templateID, params)
     .then(res=>{  
